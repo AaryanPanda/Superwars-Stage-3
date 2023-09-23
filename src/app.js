dffd8fa7df3059fc2ef7fa3ff3f9093600ff9322
aaryan <aaryanpanda04@gmail.com>
2023-09-23 12:50:40 +0530

    Worked on app.js for Functionality

--- src/app.js
+++ src/app.js
@@ -21,31 +21,87 @@ const PLAYERS = [
     "Slingo"
 ];
 
-// initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = '';
 
-    // Instead of forloop use Map method
-    // Code here
 
-    return detailedPlayers;
-}
 
 // getting random strength
 const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
 
+
+
+
+
+
+
+
+
+
+// initialize players with image and strength
+const initPlayers = (players) => {
+    let detailedPlayers = [];
+
+    // Instead of forloop use Map method
+    // Code here
+
+    players.map(function(player,index) {
+        let detail = {
+            name: player,
+            strength:getRandomStrength(),
+            image: `./images/super-${index+1}.png`,
+            type: index%2 === 0 ? "hero" : "villain",
+        };
+
+        detailedPlayers.push(detail);
+
+    })
+
+    return detailedPlayers;
+}
+
+
+
+
+
+
+
+
+
+
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let fragment = "";
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
 
+    fragment = players
+    .filter((player) => player.type === type)
+    .map(
+      (player) => `<div class="player">
+        <img src="${player.image}" alt="">
+        <div class="name">${player.name}</div>
+        <div class="strength">${player.strength}</div></div>`
+    )
+    .join("")
+
+    
+
     return fragment;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 // Display players in HTML
 const viewPlayers = (players) => {
